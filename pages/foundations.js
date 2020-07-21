965720ca60a5f5f03c27a301ce0b04e420bf7c3e
Michael Auer <miauer@icloud.com>
2020-07-21 15:19:10 -0400

    Add foundations to page

--- pages/foundations.js
+++ pages/foundations.js
@@ -3,7 +3,20 @@ import Layout from '../components/layout'
 const Foundations = () => (
   <Layout>
     <h1 className="title">Foundations</h1>
-    <p className="description">Nothing here yet.</p>
+    <div className="grid">
+      <a href="/posts/accessibility" className="card">
+        <h3>Accessibility &rarr;</h3>
+      </a>
+      <a href="/posts/color" className="card">
+        <h3>Color &rarr;</h3>
+      </a>
+      <a href="/posts/iconography" className="card">
+        <h3>Iconography &rarr;</h3>
+      </a>
+      <a href="/posts/typography" className="card">
+        <h3>Typography &rarr;</h3>
+      </a>
+    </div>
   </Layout>
 )
 
